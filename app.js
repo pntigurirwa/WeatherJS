aee1e6c9862c4ed6ed51a1b5c3f5188e8da17660
pntigurirwa <pntigurirwa@gmail.com>
2020-05-30 12:49:35 -0500

    display the weather

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init weather object
 const weather = new Weather("Boston", "MA");
 
+// Init UI
+const ui = new UI();
+
 // Get weather on DOM load
 document.addEventListener("DOMContentLoaded", getWeather);
 
