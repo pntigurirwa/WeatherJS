0996d7860ce8fe6d1e6c951b1e2756502ee52c2b
pntigurirwa <pntigurirwa@gmail.com>
2020-05-31 12:54:51 -0500

    save location to local storage

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+// Initialize storage
+
+const storage = new Storage();
+// Get storage location data
+const weatherLocation = storage.getLocationData();
+
 // Init weather object
-const weather = new Weather("Boston", "MA");
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 
 // Init UI
 const ui = new UI();
@@ -7,7 +13,25 @@ const ui = new UI();
 // Get weather on DOM load
 document.addEventListener("DOMContentLoaded", getWeather);
 
-// weather.changeLocation('Miami', 'FL');
+// Change Location Event
+document.getElementById('w-change-btn').addEventListener('click', (e)
+    => {
+    const city = document.getElementById('city').nodeValue;
+    const state = document.getElementById('state').nodeValue;
+
+    // Change Location 
+    weather.changeLocation(city, state);
+
+    // Change Location in Local storage
+    storage.setLocationData(city, state); 
+    // Get and display weather
+    getWeather();
+
+    // close modal
+    $('#locModal').modal('hide');
+    });
+
+
 
 function getWeather() {
   weather
